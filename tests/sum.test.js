0656c8096bd68a74ee31cf06f6f1af119b6e1e52
ShimeSaba1144 <bluetree.yuuyuu.0901@gmail.com>
2024-04-16 12:38:43 +0900

    test2
    test2

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -2,4 +2,4 @@
 const sum = require('./sum'); // テスト対象のモジュールをインポートする
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3); // テストコード
+  expect(sum(1, 2)).toBe(3); // テストコードchmod +x node_modules/.bin/jest
